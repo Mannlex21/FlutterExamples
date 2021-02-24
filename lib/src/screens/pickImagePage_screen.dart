522b8274ee4351efc9a3023c6373ccd0a173752e
mannlexx21 <mmurillo@syesoftware.com>
2021-02-23 22:11:00 -0600

    pick image was added

--- lib/src/screens/pickImagePage_screen.dart
+++ lib/src/screens/pickImagePage_screen.dart
@@ -1,13 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class PickImagePageScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PickImagePageScreen extends StatefulWidget {
+  const PickImagePageScreen({Key key}) : super(key: key);
+
+  @override
+  _PickImagePageScreen createState() => _PickImagePageScreen();
+}
+
+class _PickImagePageScreen extends State<PickImagePageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Uso de Pick image"),
       ),
-      body: Center(),
+      body: Center(
+        child: getImageWidget(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _showOptions(context),
+      ),
     );
+  }
+
+  Widget getImageWidget() {
+    if (_image == null) {
+      return Text("Seleccione una imagen");
+    } else {
+      return Image.file(_image);
+    }
+  }
+
+  File _image;
+
+  void getImage(ImageSource source) async {
+    var image = await ImagePicker().getImage(source: source);
+
+    setState(() {
+      _image = File(image.path);
+      Navigator.pop(context);
+    });
+  }
+
+  void _showOptions(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            children: [
+              ListTile(
+                title: Text("Cámara"),
+                leading: Icon(Icons.camera_alt),
+                onTap: () => getImage(ImageSource.camera),
+              ),
+              ListTile(
+                title: Text("Gallery"),
+                leading: Icon(Icons.photo),
+                onTap: () => getImage(ImageSource.gallery),
+              )
+            ],
+          );
+        });
   }
 }
